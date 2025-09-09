be5168c1f06e78bd6a595bf5740d6442432d2a23
Victor <sangmu.kim@gmail.com>
2025-09-09 11:38:14 -0700

    fix(experience)

--- pages/index.js
+++ pages/index.js
@@ -22,35 +22,65 @@ export default function Home() {
      <div>
       {/* Hero */}
       <section id="hero" className="hero">
-        <img src="/logo.png" alt="Victor caricature logo" className="logo" />
-        <div>
-          <h1>Hi, I’m Victor</h1>
-          <p>Full-Stack Engineer</p>
-          <a href="https://github.com/">GitHub</a> |{" "}
-          <a href="https://linkedin.com/">LinkedIn</a> {" "}
+  <img src="/logo.png" alt="Victor caricature logo" className="logo" />
+  <div>
+    <h1>Hi, I’m Victor</h1>
+    <p>Full-Stack Engineer</p>
+    <div className="hero-links">
+      <a href="https://github.com/starsang45" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <a href="https://www.linkedin.com/in/victorsang/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      <a href="https://docs.google.com/document/d/11d8kFb0qy8OWbm0way6ZJd-JRbS5d8IT0x6DP9TJQug/edit?usp=sharing" target="_blank">Resume</a>
+    </div>
+  </div>
+</section>
 
-        </div>
-      </section>
 
       {/* Experience */}
       <section id="experience">
-        <h2>Experience</h2>
-        <div className="card">
-          <h3>Company A</h3>
-          <p>Software Engineer (2023 – Present)</p>
-          <p>
-            Produced esports live stream → $450K revenue / 1.2M concurrent
-            viewers
-          </p>
-        </div>
-      </section>
+  <h2>Experience</h2>
+
+  <div className="card experience-card">
+    <div className="exp-item">
+      <span className="exp-title">OSLabs | </span>
+      <span className="exp-date">May 2025 – Present</span>
+    </div>
+    <p className="exp-role">Software Engineer</p>
+  </div>
+
+  <div className="card experience-card">
+    <div className="exp-item">
+      <span className="exp-title">Open Source | </span>
+      <span className="exp-date">Mar 2023 – Jul 2025</span>
+    </div>
+    <p className="exp-role">Software Engineer</p>
+  </div>
+
+  <div className="card experience-card">
+    <div className="exp-item">
+      <span className="exp-title">KBS | </span>
+      <span className="exp-date">Mar 2023 – Jul 2025</span>
+    </div>
+    <p className="exp-role">Digital Content Manager</p>
+  </div>
+
+  {/* ... */}
+</section>
+
 
       {/* Education */}
       <section id="education">
         <h2>Education</h2>
         <div className="card">
-          <h3>Oxford University</h3>
-          <p>MBA, 2021</p>
+          <h3>Georgia Institute of Technology</h3>
+          <p>Master of Computer Science, 2023 - present</p>
+        </div>
+        <div className="card">
+          <h3>University of Oxford</h3>
+          <p>MBA, 2005 - 2006</p>
+        </div>
+        <div className="card">
+          <h3>Hanyang Cyber University</h3>
+          <p>B.S.in Computer Science, 2021 - 2023</p>
         </div>
       </section>
 
