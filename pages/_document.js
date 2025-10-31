ec84fe88cc647a928e572e1f240a963512d84e2f
Victor <sangmu.kim@gmail.com>
2025-10-30 19:44:57 -0700

    add meta

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* ✅ Google Search Console Verification Meta Tag */}
+        <meta
+          name="google-site-verification"
+          content="googlea44d2f8200ef0a29.html"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
