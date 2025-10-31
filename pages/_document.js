44d5850dc59a546cbdfbd30a2834c3aad4526af3
Victor <sangmu.kim@gmail.com>
2025-10-31 11:05:14 -0700

    fix meta again

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,21 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* ✅ Google Analytics 4 (GA4) Tracking Code */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-QBXB69MXE7"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-QBXB69MXE7');
+            `,
+          }}
+        />
         {/* ✅ Google Search Console Verification Meta Tag */}
         <meta
           name="google-site-verification"
